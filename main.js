fee38b03cf1781d7dd8b921d296d0846966be3c6
Masayuki Onishi <omccello@gmail.com>
2022-03-19 14:25:54 +0900

    set base objects

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 import * as THREE from './build/three.module.js';
+//
+//
+// ###########################
+// 1. Base Materials
+// ###########################
+//
+//
+//
+// Define Scene
+//
+let scene = new THREE.Scene();
+//
+// Define Camera
+//
+let camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+//
+// Define Renderer
+//
+let renderer = new THREE.WebGLRenderer();
 
-// Confirm Scene
-const scene = new THREE.Scene();
-console.log(scene);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
